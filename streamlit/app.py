0cf022c974d5a980df87ecb536a816ed27570acb
Khushiyant <khushiyant2002@gmail.com>
2023-08-28 22:04:24 +0530

    chore: langchain--helpers

---
+++ streamlit/app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from utils.helpers import ColdMailing, PDFTextExtractor
+
+extractor = PDFTextExtractor()
+mailer = ColdMailing()
+
+
+st.title("Cold Letter")
+st.write("Cold Letter is a tool to help you write cold emails. It scrapes the web for information about a company or professor and generates a template for you to use.")
+type = st.radio("What would you like to do?", ("Company", "Professor"))
+
+
+st.header("Target Information")
+target_name = st.text_input("Name", placeholder="Google")
+requested_position = st.text_input("Position", placeholder="Software Engineer")
+
+st.header("Your Information")
+name = st.text_input("Your Name", placeholder="John Doe")
+
+st.header("Resume Upload")
+resume = st.file_uploader("Upload your resume", type="pdf")
+
+# Submit button
+if st.button("Submit"):
+    content = mailer.generate(target_name, name, extractor.extract(resume), type, requested_position)
+    st.write(content)
+    
---
+++ streamlit/utils/helpers.py
@@ -0,0 +1,82 @@
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+from langchain.llms import OpenAI
+import PyPDF2
+from langchain.tools import Tool
+from langchain.utilities import GoogleSearchAPIWrapper
+from dotenv import load_dotenv
+
+
+# Load environment variables
+load_dotenv()
+
+
+class ColdMailing:
+    """
+    Generates a cold email template.
+
+    Parameters
+    ----------
+    company_name : str
+    name : str
+    email : str
+    resume : file
+    """
+
+    def __init__(self) -> None:
+        self.llm = OpenAI(temperature=0.6, max_tokens=1000)
+
+        search = GoogleSearchAPIWrapper(k=1)
+
+        self.tools = [
+            Tool(
+                name="ArXiv Search",
+                description="Search research papers about professor.",
+            ),
+            Tool(
+                name="Google Search",
+                description="Search Google for recent results about the company or professor. Use it only once",
+                func=search.run,
+            )
+        ]
+
+        self.agent = initialize_agent(
+            self.tools, self.llm,
+            agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+            verbose=True)
+
+    def generate(self, target_name, name, resume, type, requested_position):
+        self.name = name
+        self.target_name = target_name
+        self.resume = resume
+        return self.agent.run(f"""
+            Write a cold mail to {type} named {target_name} for the position of {requested_position} with following conditons:
+            if it is a professor then use his/her top 3 best work by searching google scholar.
+
+            Use the following information to create the cold mail:
+            {resume}
+            """)
+
+
+class PDFTextExtractor:
+    """
+    Extracts text from a PDF file.
+
+    Parameters
+    ----------
+    file : file
+        The PDF file to extract text from.
+
+        Returns
+        -------
+        text : str
+    """
+
+    def extract(self, file):
+        text = str()
+        fileReader = PyPDF2.PdfReader(file)
+        for page in fileReader.pages:
+            text += page.extract_text()
+
+        return text
